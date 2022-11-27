280d98afb33e35a7b7b53a329dec263620f6fb12
William Kenzie <46734500+WillKenzie@users.noreply.github.com>
2022-11-27 16:19:32 -0600

    Signed-off-by: William Kenzie <46734500+WillKenzie@users.noreply.github.com>

--- src/lib/Policies/HistoryPolicy.py
+++ src/lib/Policies/HistoryPolicy.py
@@ -14,10 +14,24 @@ class HistoryPolicy(Policy):
         self.name = "HistoryPolicy"
         self.active = False
         self.kw: KnowWhere = gmget("knowwhere")
+        self.platform = self.kw.platform
+
+    def _get_history_internal(self, browser_path):
+        from sqlite3 import connect, OperationalError
+        try:
+            db = connect(browser_path)
+
+            # noinspection SqlNoDataSourceInspection
+            entries = db.execute("SELECT term from keyword_search_terms").fetchall()
+            db.close()
+            for entry in entries:
+                entry = entry[0]
+                if _BWLIST.check(entry):
+                    gmget("reporter").report(entry)
+        except OperationalError:
+            return
 
     def _get_history(self):
-        from sqlite3 import connect, OperationalError
-        from platform import system
         windows_supported_browsers = [
             self.kw.appdata + "/Microsoft/Edge/User Data/Default/History",
             self.kw.appdata + "/AVG/Browser/User Data/Default/History",
@@ -28,18 +42,22 @@ class HistoryPolicy(Policy):
             self.kw.appdata + "/Google/Chrome/User Data/Default/History",
             self.kw.appdata + "/../Roaming/Opera Software/Opera Stable/History",
         ]
-        try:
-            if system() == 'Windows':
-                for browser_path in windows_supported_browsers:
-                    db = connect(browser_path)
 
-                    # noinspection SqlNoDataSourceInspection
-                    entries = db.execute("SELECT term from keyword_search_terms").fetchall()
-                    db.close()
-                    for entry in entries:
-                        entry = entry[0]
-                        if _BWLIST.check(entry):
-                            gmget("reporter").report(entry)
+        posix_supported_browsers = [
+            self.kw.appdata + "/microsoft-edge/Default/History",
+            self.kw.appdata + "/vivaldi/Default/History",
+            self.kw.appdata + "/brave/Default/History",
+            self.kw.appdata + "/chromium/Default/History",
+            self.kw.appdata + "/google-chrome/Default/History",
+            self.kw.appdata + "/opera/History",
+        ]
+        try:
+            if self.platform == "nt":
+                for browser_path in windows_supported_browsers:
+                    self._get_history_internal(browser_path)
+            elif self.platform == "posix":
+                for browser_path in posix_supported_browsers:
+                    self._get_history_internal(browser_path)
 
         except OperationalError as e:
             print(e)
